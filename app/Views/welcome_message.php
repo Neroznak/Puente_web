06ed4abb915b703d220fd56aea2c97d980301e23
NeroznakD <neroznakdn@gmail.com>
2023-11-12 11:12:28 +0100

    init

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -220,7 +220,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to CodeIgniter Neroznak <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+        <h1>Welcome to CodeIgniter Neroznak Dmitrii <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
         <h2>The small framework with powerful features</h2>
 
