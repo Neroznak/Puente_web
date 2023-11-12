12a661eeefb8f3da6e9b4a21f709138bddcf0d14
NeroznakD <neroznakdn@gmail.com>
2023-11-12 13:24:04 +0100

    create header, start to create lending

---
+++ app/Views/templates/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <title>Puente</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="head col-lg-12 text-right center-text"><a href="/">Войти</a> / <a href="/">Регистрация</a></div>
+<img src="assets/img/рабместо.jpg" alt="">
+<div class="div">Облачная система контроля за строительной организацией
+    <input type="submit" class="btn btn-default" value="Начать сейчас"> </div>
+
+<div id="blc1" class="div col-lg-4">40 часов</div>
+<div id="blc2" class="div col-lg-4">+ 160 раз</div>
+<div id="blc3" class="div col-lg-4">80%</div>
+
+</body>
+</html>
+
