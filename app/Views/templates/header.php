0b822d56258be798cac8a0bf684c8095abaab3b4
NeroznakD <neroznakdn@gmail.com>
2023-11-14 19:12:02 +0100

    lending v.1 Всё готово, дальше только улучшать

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -4,193 +4,278 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Pragma" content="no-cache"/>
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
     <title>Puente</title>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
 </head>
 <body>
 <!--Хэдер-->
-<div class="head col-lg-12 text-right center-text">
-    <a href="/">Войти</a> <a href="/">Регистрация</a>
+<div class="head col-lg-12">
+    <div class="container">
+        <div class="up-btn">
+            <a class="lf-btn" href="/">Войти</a>
+            <a href="/">Регистрация</a>
+        </div>
+    </div>
 </div>
 <!--Конец хэдера-->
+
 <div class="blok">
     <div class="opacity">
-        <div class="lkkl">Облачная система контроля за строительной организацией
-            <input type="submit" class="btn btn-nero" value="Начать сейчас">
+        <div class="container">
+            <div class="lkkl">Облачная система контроля за строительной организацией
+                <input type="submit" class="btn btn-nero" value="Начать сейчас">
+            </div>
         </div>
-        <div class="blc">
-            <div class="blc1 col-lg-4">40 часов
-                <p>система автоматизирует 40 часов работы сотрудников в неделю</p></div>
-            <div class="blc1 col-lg-4">+ 160 раз
-                <p>подготовка исполнительной документации быстрее в 160 раз</p></div>
-            <div class="blc1 col-lg-4">80%
-                <p>получай оплату на 80% быстрее, чем при ручном контроле</p></div>
-        </div>
+    </div>
+</div>
 
+<div class="blc col-lg-12">
+    <div class="container">
+        <div class="blc1 col-lg-4">40 часов<p>система автоматизирует 40 часов работы сотрудников в неделю</p>
+        </div>
+        <div class="blc1 col-lg-4">+ 160 раз<p>подготовка исполнительной документации быстрее в 160 раз</p>
+        </div>
+        <div class="blc1 col-lg-4">80%<p>получай оплату на 80% быстрее, чем при ручном контроле</p></div>
     </div>
 </div>
+
 <!--Конец стартовой страницы-->
-<div class="page2">
-    <div class="page2-left col-lg-4">
-        <div class="blc2">
-            ВАШ БИЗНЕС МОЖЕТ БОЛЬШЕ
+<div class="page2 col-lg-12">
+    <div class="container">
+        <div class="page2-left col-lg-2 col-lg-pull-1">
+            <div class="blc2">
+                ВАШ БИЗНЕС МОЖЕТ БОЛЬШЕ
+            </div>
+            <div class="comm1">
+                <h4>Достигайте амбициозных целей с помощью простой системы.</h4>
+                <img src="./assets/css/galochka.png" alt="Галочка">
+            </div>
+            <div class="comm1">
+                <h4>Быстрые и грамотные решения снижают стоимость строительства</h4>
+                <img src="./assets/css/galochka.png" alt="Галочка">
+            </div>
+            <div class="comm1">
+                <h4>Уберём бюрократию, оставим только автоматизированный контроль!</h4>
+                <img src="./assets/css/galochka.png" alt="Галочка">
+            </div>
         </div>
-        <div class="comm1">
-            Достигайте амбициозных целей с помощью простой системы.
-        </div>
-        <div class="comm1">
-            <h5>С нашим приложением все бизнес процессы под контролем. Теперь Вы сможете быстрее принимать решение и снизить стоимость производства работ!</h5>
-        </div>
-        <div class="comm1">
-            Уберём бюрократию, оставим только автоматизированный контроль!
-        </div>
+        <div class="page2-right col-lg-2 col-lg-push-5">
+            <img src="./assets/css/erp.png" alt="Схема"></div>
     </div>
 </div>
+
 
 <div class="col-lg-12 ner-blc">
-    <h3>EL PUENTE поможет Вам в развитии бизнеса. В одном месте мы собрали все основные направления работы, чтобы они
-        заговорили на одном языке.</h3>
-    <div class="whitblc col-lg-1">
-        <h1>УП</h1>
-        <h4>Управление проектами</h4>
-    </div>
-    <div class="whitblc col-lg-2">
-        <h1>ЦС</h1>
-        <h4>Цели и стратегии</h4>
-    </div>
-    <div class="whitblc col-lg-3">
-        <h1>УР</h1>
-        <h4>Управление ресурсами</h4>
-    </div>
-    <div class="whitblc col-lg-4">
-        <h1>РК</h1>
-        <h4>Работа с контрагентами</h4>
-    </div>
-    <div class="whitblc col-lg-5">
-        <h1>УЗ</h1>
-        <h4>Управление задачами</h4>
-    </div>
-    <div class="whitblc col-lg-6">
-        <h1>ЮО</h1>
-        <h4>Юридический отдел</h4>
-    </div>
-    <div class="whitblc col-lg-7">
-        <h1>БУ</h1>
-        <h4>Бухгалтерия</h4>
-    </div>
-</div>
-
-<div class="col-lg-12 text-center">
-    <h1>Поговорим о деталях</h1>
-    <div class="detals col-lg-8"><h4>Цели достигаются быстрее, если они встроены в систему контроля</h4>
-        <hr>
-        <h6>Анализ проектов на соответствие общим целям</h6>
-        <h6>Советы по достижению целей</h6>
-        <h6>Наглядная статистика, адаптированная под цели</h6>
-        <h6>Прогноз достижения целей</h6>
+    <div class="container">
+        <h3 class="text-center">EL PUENTE поможет Вам в развитии бизнеса. В одном месте мы собрали все основные
+            направления
+            работы, чтобы
+            они
+            заговорили на одном языке.</h3>
+        <div class="row">
+            <div class="whitblc col-lg-1">
+                <h1>УП</h1>
+                <h4>Управление проектами</h4>
+            </div>
+            <div class="whitblc col-lg-1">
+                <h1>ЦС</h1>
+                <h4>Цели и стратегии</h4>
+            </div>
+            <div class="whitblc col-lg-1">
+                <h1>УР</h1>
+                <h4>Управление ресурсами</h4>
+            </div>
+            <div class="whitblc col-lg-1">
+                <h1>РК</h1>
+                <h4>Работа с контрагентами</h4>
+            </div>
+            <div class="whitblc col-lg-1">
+                <h1>УЗ</h1>
+                <h4>Управление задачами</h4>
+            </div>
+            <div class="whitblc col-lg-1">
+                <h1>ЮО</h1>
+                <h4>Юридический отдел</h4>
+            </div>
+            <div class="whitblc col-lg-1">
+                <h1>БУ</h1>
+                <h4>Бухгалтерия</h4>
+            </div>
+        </div>
     </div>
 </div>
 
+<div class="col-lg-12">
+    <div class="container">
+        <h1 class="text-center">Поговорим о деталях</h1>
 
-<div class="col-lg-12 text-center">
-    <div class="detals col-lg-8"><h4>Гораздо больше, чем «проекты, которые заботятся сами о себе»</h4>
-        <hr>
-        <h6>Информативная и удобная панель управления</h6>
-        <h6>Авторекомендация по закупкам материала</h6>
-        <h6>Автосоставление исполнительной документации</h6>
-        <h6>Планирование ресурсов строительства</h6>
+        <div class="blc-info1 col-lg-12">
+            <hr>
+            <div class="detals col-lg-12"><h4 class="text-right">Цели достигаются быстрее, если они встроены в систему
+                    контроля</h4>
+                <img class="col-lg-4" src="./assets/css/sp.png" alt="Настройки цели">
+                <div class="txt col-lg-4 col-lg-push-2">
+                    <div class="kub"> Анализ проектов на соответствие общим целям</div>
+                    <div class="kub"> Советы по достижению целей</div>
+                    <div class="kub">Наглядная статистика, адаптированная под цели</div>
+                    <div class="kub">Прогноз достижения целей</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="blc-info2 col-lg-12">
+            <hr>
+            <div class="detals col-lg-12"><h4 class="text-left">Гораздо больше, чем «проекты, которые заботятся сами о
+                    себе»</h4>
+                <img class="col-lg-6 col-lg-push-6" src="./assets/css/kp.png" alt="КП">
+                <div class="txt col-lg-4 col-lg-pull-6">
+                    <div class="kub">Информативная и удобная панель управления</div>
+                    <div class="kub">Авторекомендация по закупкам материала</div>
+                    <div class="kub">Автосоставление исполнительной документации</div>
+                    <div class="kub">Планирование ресурсов строительства</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="blc-info2 col-lg-12">
+            <hr>
+            <div class="detals col-lg-12"><h4 class="text-right">Повышайте эффективность
+                    работы Ваших сотрудников</h4>
+                <img class="col-lg-6" src="./assets/css/kp.png" alt="КП">
+                <div class="txt col-lg-4 col-lg-push-2">
+                    <div class="kub">Удобное распределение задачи внутри отделов</div>
+                    <div class="kub">Эффективный контроль за KPI</div>
+                    <div class="kub">Справедливая кадровая политика</div>
+                    <div class="kub">Поручения больше не будут забыты</div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="blc-info2 col-lg-12">
+            <hr>
+            <div class="detals col-lg-12"><h4 class="text-left">Автоматизм в управлении ресурсами с нашим
+                    приложением</h4>
+                <img class="col-lg-6 col-lg-push-6" src="./assets/css/kp.png" alt="КП">
+                <div class="txt col-lg-4 col-lg-pull-6">
+                    <div class="kub">Автоподбор оптимальных контрагентов</div>
+                    <div class="kub">Контроль за ресурсами предприятия</div>
+                    <div class="kub">Закупки включенные в систему бухгалтерии</div>
+                    <div class="kub">Не теряйте оптимальных поставщиков</div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="blc-info2 col-lg-12">
+            <hr>
+            <div class="detals col-lg-12"><h4 class="text-right">Вся история взаимодействия с контрагентом на одной
+                    странице!</h4>
+                <img class="col-lg-6" src="./assets/css/kp.png" alt="КП">
+                <div class="txt col-lg-4 col-lg-push-2">
+                    <div class="kub">Удобная панель взаимодействия</div>
+                    <div class="kub">Прогноз поведения</div>
+                    <div class="kub">Взаимоучёт количественных величин</div>
+                    <div class="kub">Система рейтинговой оценки контрагента</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="blc-info2 col-lg-12">
+            <hr>
+            <div class="detals col-lg-12"><h4 class="text-left">Составляйте документы в своих интересах
+                    автоматически</h4>
+                <img class="col-lg-6 col-lg-push-6" src="./assets/css/kp.png" alt="КП">
+                <div class="txt col-lg-4 col-lg-pull-6">
+                    <div class="kub">Редактор договоров и соглашений</div>
+                    <div class="kub">Советы по качественным формулировкам</div>
+                    <div class="kub">Не пропустите важные положения</div>
+                    <div class="kub">Отстаивайте свои интересы</div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
-
-<div class="col-lg-12 text-center">
-    <div class="detals col-lg-8"><h4>Повышайте эффективность
-            работы Ваших сотрудников</h4>
-        <hr>
-        <h6>Удобное распределение задачи внутри отделов</h6>
-        <h6>Эффективный контроль за KPI</h6>
-        <h6>Справедливая кадровая политика</h6>
-        <h6>Поручения больше не будут забыты</h6>
-    </div>
-</div>
-
-
-<div class="col-lg-12 text-center">
-    <div class="detals col-lg-8"><h4>Автоматизм в управлении ресурсами с нашим приложением</h4>
-        <hr>
-        <h6>Автоподбор оптимальных контрагентов</h6>
-        <h6>Контроль за ресурсами предприятия</h6>
-        <h6>Закупки включенные в систему бухгалтерии</h6>
-        <h6>Не теряйте оптимальных поставщиков</h6>
-    </div>
-</div>
-
-
-<div class="col-lg-12 text-center">
-    <div class="detals col-lg-8"><h4>Вся история взаимодействия с контрагентом на одной странице!</h4>
-        <hr>
-        <h6>Удобная панель взаимодействия</h6>
-        <h6>Прогноз поведения</h6>
-        <h6>Взаимоучёт количественных величин</h6>
-        <h6>Система рейтинговой оценки контрагента</h6>
-    </div>
-</div>
-
-
-<div class="col-lg-12 text-center">
-    <div class="detals col-lg-8"><h4>Составляйте документы в своих интересах автоматически</h4>
-        <hr>
-        <h6>Редактор договоров и соглашений</h6>
-        <h6>Советы по качественным формулировкам</h6>
-        <h6>Не пропустите важные положения</h6>
-        <h6>Отстаивайте свои интересы</h6>
-    </div>
-</div>
 <div class="col-lg-12 nerd-blc">
-    <div class="col-lg-1">
-        <img src="./assets/css/man.png" alt="гендир"></div>
-    <div class="text-left col-lg-1 col-lg-push-3">
-        <h1>Благодаря Puente вся компания говорит на одном языке.</h1>
-        <h2>Dmitrii Neroznak</h2>
-        <h2> Генеральный директор Neroznak IT.</h2>
+    <div class="container">
+        <div class="col-lg-1">
+            <img src="./assets/css/man.png" alt="гендир"></div>
+        <div class="text-left col-lg-1 col-lg-push-3">
+            <h1>Благодаря Puente вся компания говорит на одном языке.</h1>
+            <h2>Dmitrii Neroznak</h2>
+            <h2> Генеральный директор Neroznak IT.</h2>
+        </div>
     </div>
 </div>
+
+
+<h1 class="text-center col-lg-12">Популярные вопросы</h1>
+<h4 class="text-center col-lg-12">Какая система лучше всего подходит для малого и среднего бизнеса?</h4>
+<h4 class="text-center col-lg-12">Что такое облачная система ERP-программное обеспечение?</h4>
+<h4 class="text-center col-lg-12">Почему мне следует внедрить ERP-решение в своём бизнесе?</h4>
+<h4 class="text-center col-lg-12">В чём разница между ERP и CRM?</h4>
+<h4 class="text-center col-lg-12">Как ERP-система повышает удовлетворённость клиентов?</h4>
+
 
 <div class="tarifs col-lg-12">
-    <h1>Тарифы</h1>
-    <div class="trf-1 col-lg-3">
-        <h1>Light</h1>
-        <h4>Управление проектами</h4>
-        <h4>Управление сотрудниками</h4>
-        <h4>Управление задачами</h4>
-        <h2 class="text-center">39 $ per month</h2>
-    </div>
-    <div class="trf-1 col-lg-6">
-        <h1>Medium</h1>
-        <h4>Управление проектами</h4>
-        <h4>Управление сотрудниками</h4>
-        <h4>Управление задачами</h4>
-        <h4>Работа с контрагентами</h4>
-        <h4>Юридический отдел</h4>
-        <h2 class="text-center">69 $ per month</h2>
 
-    </div>
-    <div class="trf-1 col-lg-9">
-        <h1>Max</h1>
-        <h4>Управление проектами</h4>
-        <h4>Управление сотрудниками</h4>
-        <h4>Управление задачами</h4>
-        <h4>Работа с контрагентами</h4>
-        <h4>Юридический отдел</h4>
-        <h4>Тендерный отдел</h4>
-        <h4>Планирование</h4>
-        <h2 class="text-center">99 $ per month</h2>
+    <div class="container">
+        <h1>Тарифы</h1>
+        <div class="row">
+
+            <div class="trf-1 col-lg-3">
+                <h1>Light</h1>
+                <h4>Управление проектами</h4>
+                <h4>Управление сотрудниками</h4>
+                <h4>Управление задачами</h4>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <h2 class="text-center">39 $ per month</h2>
+                <input type="submit" class="btn-nero2" value="Купить">
+            </div>
+
+            <div class="trf-1 col-lg-6">
+                <h1>Medium</h1>
+                <h4>Управление проектами</h4>
+                <h4>Управление сотрудниками</h4>
+                <h4>Управление задачами</h4>
+                <h4>Работа с контрагентами</h4>
+                <h4>Юридический отдел</h4>
+                <br>
+                <br>
+                <br>
+                <h2 class="text-center">69 $ per month</h2>
+                <input type="submit" class="btn-nero3" value="Купить">
+
+            </div>
+
+            <div class="trf-1 col-lg-9">
+                <h1>Max</h1>
+                <h4>Управление проектами</h4>
+                <h4>Управление сотрудниками</h4>
+                <h4>Управление задачами</h4>
+                <h4>Работа с контрагентами</h4>
+                <h4>Юридический отдел</h4>
+                <h4>Тендерный отдел</h4>
+                <h4>Планирование</h4>
+                <h2 class="text-center">99 $ per month</h2>
+                <input type="submit" class="btn-nero4" value="Купить">
+            </div>
+
+        </div>
     </div>
 </div>
+
+
 <div class="margin-8"></div>
+
+
 <!--<div class="row">-->
 <div class="col-lg-12 footr">
     <p>Designed by Neroznak studio</p>
